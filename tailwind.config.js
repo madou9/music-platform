c14bd47da46db48afe12558ce21190f223c32246
madou9 <issoufou.mamadou@gmail.com>
2025-04-18 01:16:30 +0200

    moved the asset to the public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue, js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+    },
     extend: {},
   },
   plugins: [],
