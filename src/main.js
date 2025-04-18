784dbda64a237d960d95639b1c9a7313b72fd761
madou9 <issoufou.mamadou@gmail.com>
2025-04-18 23:21:52 +0200

    added plugin for veeValidation

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/base.css";
 import "./assets/main.css";
+import VeeValidatePlugin from "./includes/validation";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
