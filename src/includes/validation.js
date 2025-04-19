80146dcb05d91a609a587ce982425d7ecd5eb1ea
madou9 <issoufou.mamadou@gmail.com>
2025-04-19 15:54:47 +0200

    added some rule for validation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,11 +1,35 @@
-import { Form as VeeForm, Field as VeeField, defineRule } from "vee-validate";
-import { required } from "@vee-validate/rules";
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+} from "vee-validate";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaspaces,
+  email,
+  min_value as minVal,
+  max_value as maxVal,
+  confirmed,
+  not_one_of as excluded,
+} from "@vee-validate/rules";
 
 export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaspaces);
+    defineRule("email", email);
+    defineRule("min_value", minVal);
+    defineRule("max_value", maxVal);
+    defineRule("confirmed", confirmed);
+    defineRule("excluded", excluded);
   },
 };
