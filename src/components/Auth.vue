5ee4812dd47fa2a34261321efe1bb9bb0367d5ef
madou9 <issoufou.mamadou@gmail.com>
2025-04-19 14:19:30 +0200

    added name to the vee field component

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -96,6 +96,7 @@
               <label class="inline-block mb-2">Name</label>
               <vee-field
                 type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
