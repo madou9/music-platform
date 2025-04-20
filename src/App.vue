b853adefd52bcd98c4b6f2d94fcae11130680398
madou9 <issoufou.mamadou@gmail.com>
2025-04-20 18:40:07 +0200

    checked if the user has authentication session with firebase before vue application

--- src/App.vue
+++ src/App.vue
@@ -275,12 +275,23 @@
 <script>
 import Auth from "./components/Auth.vue";
 import AppHeader from "./components/AppHeader.vue";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { auth } from "./includes/firebase";
 
 export default {
   name: "App",
   components: {
     AppHeader,
     Auth,
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
   },
 };
 </script>
