9cea9fd708005536f1d8df28a032b255776df354
madou9 <issoufou.mamadou@yahoo.com>
2025-04-22 21:12:08 +0200

    added route path

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,16 +3,17 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
-        >Music</router-link
+      <router-link class="text-white font-bold uppercase text-2xl mr-4"
+      exact-active-class="no-active" :to="{name: 'home'}">
+        Music</router-link
       >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
-          <li v-if="!userStore.userLoggedIn">
-            <router-link class="px-2 text-white" to="/about">about</router-link>
+          <li v-if="userStore.userLoggedIn">
+            <router-link class="px-2 text-white" to="/about">About</router-link>
           </li>
 
           <li v-if="!userStore.userLoggedIn">
@@ -22,7 +23,7 @@
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage"
+              <router-link class="px-2 text-white" :to="{name: 'manage'}"
                 >Manage</router-link
               >
             </li>
