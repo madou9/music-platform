08f356614992ff113a1b30123208cf647c0577b3
madou9 <issoufou.mamadou@yahoo.com>
2025-04-22 21:13:01 +0200

    added catch path to redirect the user to the home path if the path doesn't exist

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,34 @@ import ManageView from "@/views/ManageView.vue";
 
 const routes = [
   {
+    name:"home",
     path: "/",
     component: HomeView,
   },
   {
+    name:"about",
     path: "/about",
     component: AboutView,
   },
   {
-    path: "/manage",
+    name:"manage",
+    path: "/manage-music",
     component: ManageView,
+  },
+  {
+    path: "/manage",
+    redirect:{name: 'manage'},
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect:{name: 'home'},
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
