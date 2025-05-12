f0a16b6916c1b32986f4fc5589380c36a4eb7d65
madou9 <issoufou.mamadou@gmail.com>
2025-05-12 23:47:24 +0200

    added path for song with the id

--- src/router/index.js
+++ src/router/index.js
@@ -2,52 +2,58 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import ManageView from "@/views/ManageView.vue";
-import userUserStore from  "@/stores/user";
+import userUserStore from "@/stores/user";
+import SongView from "@/views/SongView.vue";
 
 const routes = [
   {
-    name:"home",
+    name: "home",
     path: "/",
     component: HomeView,
   },
   {
-    name:"about",
+    name: "about",
     path: "/about",
     component: AboutView,
   },
   {
-    name:"manage",
+    name: "manage",
     path: "/manage-music",
     component: ManageView,
   },
   {
     path: "/manage",
-    redirect:{name: 'manage'},
+    redirect: { name: "manage" },
   },
   {
-    path: '/:catchAll(.*)*',
-    redirect:{name: 'home'},
+    name: "song",
+    path: "/song/:id",
+    component: SongView,
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  linkExactActiveClass: 'text-yellow-500',
+  linkExactActiveClass: "text-yellow-500",
 });
 
-router.beforeEach((to, from , next) => {
-  if (!to.meta.requiresAuth){
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
     next();
-    return ;
+    return;
   }
 
   const store = userUserStore();
 
-  if (store.userLoggedIn){
+  if (store.userLoggedIn) {
     next();
-  }else{
-    next({name: "home"});
+  } else {
+    next({ name: "home" });
   }
 });
 
