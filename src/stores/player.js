84227cf06f257a247db1855ffdf29138193c7ad8
madou9 <issoufou.mamadou@gmail.com>
2025-05-19 00:44:33 +0200

    implemented player progress bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -8,9 +8,14 @@ export default defineStore("player", {
     sound: {},
     seek: "00:00",
     duration: "00:00",
+    playerProgress: "0%",
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload();
+      }
+
       this.current_song = song;
 
       this.sound = new Howl({
@@ -38,6 +43,8 @@ export default defineStore("player", {
     progress() {
       this.seek = helper.formatTime(this.sound.seek());
       this.duration = helper.formatTime(this.sound.duration());
+
+      this.playerProgress = `${(this.sound.seek() / this.sound.duration()) * 100}%`;
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress);
