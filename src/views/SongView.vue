f19a1df1d21b987f033879728d253ee1beb25615
madou9 <issoufou.mamadou@gmail.com>
2025-05-18 22:28:03 +0200

    imported the player Store:

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -89,8 +90,9 @@
 
 <script>
 import { songsCollection, auth, commentsCollection } from "@/includes/firebase";
-import { mapState } from "pinia";
+import { mapState, mapActions } from "pinia";
 import userUserStore from "@/stores/user";
+import usePlayerStore from "@/stores/player";
 
 export default {
   name: "Song",
@@ -135,6 +137,7 @@ export default {
     this.getComments();
   },
   methods: {
+    ...usePlayerStore(usePlayerStore, ["newSong"]),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true;
       this.comment_show_alert = true;
